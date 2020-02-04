f518bfe4738d8d788590b3dfd7b5a460b5ea7683
Liam Hilliard <liam.h.-@hotmail.com>
2020-02-04 22:45:05 +0000

    Initial commit
    Setup with Spring Initializr.

---
+++ src/test/java/com/liam191/videoshare/stompserver/StompServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liam191.videoshare.stompserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
